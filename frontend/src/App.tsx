a520e108c64bb6d375295ceebcb21ec1ae174aba
Aldo <aldoandres045@gmail.com>
2023-06-03 18:53:40 -0300

    Se agrega estructura del frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+import InputMail from './components/Input/Input'
+import Title from './components/Title/Title'
+
+function App() {
+  return (
+    <Fragment>
+      <Title>Iniciar Sesion</Title>
+      <InputMail></InputMail>
+    </Fragment>
+  )
+}
+
+export default App
