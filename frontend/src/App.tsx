5841df16cd619aa59fb7b4b217a327547078ea72
Aldo <aldoandres045@gmail.com>
2023-06-06 12:57:15 -0300

    Agrego Landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,25 @@
-import TopMenu from './components/TopMenu';
+import TopMenu from "./components/TopMenu";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import LogIn from './pages/LogInPage/LogIn';
-import NotFound from './pages/NotFoundPage/NotFound';
-import HomePage from './pages/HomePage/HomePage';
+import LogIn from "./pages/LogInPage/LogIn";
+import NotFound from "./pages/NotFoundPage/NotFound";
+import HomePage from "./pages/HomePage/HomePage";
 
 const routes = [
   {
     path: "/home",
-    element: <HomePage/>
+    element: <HomePage />,
   },
   {
     path: "/login",
-    element: <LogIn />
+    element: <LogIn />,
   },
   {
-    path : '*',
-    element: <NotFound />
-  }
+    path: "*",
+    element: <NotFound />,
+  },
 ];
 
+//AGREGAR PARA QUE VAYA DIRECTO A HOME
 const router = createBrowserRouter(routes);
 
 function App() {
@@ -27,7 +28,7 @@ function App() {
       <TopMenu />
       <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,86 @@
-import React from 'react'
-import { Button } from 'react-bootstrap'
+import React from "react";
+import { Button } from "react-bootstrap";
+import Title from "../../components/Title/Title";
+import "./HomePage.css";
 
 function HomePage() {
   return (
-    <div>
-        <Button style={{backgroundColor:"white", borderColor:"white", color:"black", right:"130px", position:"absolute"}}>Suscripciones</Button>
-        <button type="button" className="btn btn-outline-danger position-absolute end-0">Iniciar Sesion</button>
+    <div className="container col">
+      <div className="header">
+        <button
+          type="button"
+          className="btn btn-outline-dark"
+          style={{ marginLeft: "4px" }}
+        >
+          Suscripcion
+        </button>
+        <button
+          type="button"
+          className="btn btn-outline-danger"
+          style={{ marginLeft: "10px" }}
+        >
+          Iniciar Sesion
+        </button>
+      </div>
+      <div className="content">
+        <div>
+          <Title
+            style={{
+              width: "500px",
+              marginTop: "20px",
+              display: "flex",
+              alignContent: "center",
+            }}
+          >
+            Reserva una cancha desde donde quieras
+          </Title>
+        </div>
+        <div className="subcontent" style={{ width: "400px" }}>
+          <p>
+            Encontrá tu cancha preferida para jugar con tus amigos de entre más
+            de mil establecimientos
+          </p>
+        </div>
+        <button
+          type="button"
+          className="btn btn-danger"
+          style={{ backgroundColor: "#FF604F" }}
+        >
+          Buscar
+        </button>
+        <div className="footer">
+          <div className="row" id="figuras">
+            <div className="col1" id="fig1"></div>
+            <div className="col2" id="fig2"></div>
+          </div>
+          <div
+            className="footerContent row"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "10px",
+            }}
+          >
+            <Title style={{ fontSize: "25px" }}>
+              ¿Queres publicitar tu Establecimiento?
+            </Title>
+            <p>
+              Campo de Juego te permite administrar las reservas de <br /> tus
+              canchas, aceptar pagos a través de Mercado Pago,
+              <br /> ver reportes de ingresos y mucho más.{" "}
+            </p>
+            <button
+              type="button"
+              className="btn btn-danger"
+              style={{ backgroundColor: "#FF604F", width: "15%" }}
+            >
+              Ver Opciones
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
