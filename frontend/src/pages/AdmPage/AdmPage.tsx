90beb81068f53b51a48eae6c7227396bafa32289
Andre-Leandro <andreleandrosanlorenzo@gmail.com>
2023-06-07 10:18:25 -0300

    Agregado componente tarjeta

--- frontend/src/pages/AdmPage/AdmPage.tsx
+++ frontend/src/pages/AdmPage/AdmPage.tsx
@@ -1,11 +1,32 @@
 import React from "react";
-
+import "./AdmPage.css";
+import PaymentForm from "../../components/PaymentForm/PaymentForm";
 
 
 function AdmPage() {
   return (
     <div className="container col">
-      Hola a todos
+      <div>  
+          <div className="prueba">  Tarjeta de credito </div>
+          <div className="subtexto" >  
+          <p> Se factura una cuota cada 30 días. </p>
+          <p> Se puede dar de baja en cualquier momento. </p>
+          
+          
+          </div>
+          <div> 
+              
+          </div>
+          <div className="tarjeta" >  <PaymentForm/> </div>
+
+
+
+      </div>
+
+
+
+
+      <div>  </div>
     </div>
   );
 }
