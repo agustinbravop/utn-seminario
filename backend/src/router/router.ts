bd4cf3fa96be96cb9017dc102bcf4c0b20e4a4fa
Matias-Bustamante <matybustamante151@gmail.com>
2023-06-07 21:03:05 -0300

    EndPoint de Tarjeta

--- backend/src/router/router.ts
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express' 
-import { getSuscripcion, postSuscripcion } from '../controller/suscripcion.controller'
-
-
-const router = Router() 
-//controller de suscripcion
-router.post("/suscripcion",postSuscripcion)
-router.get("/suscripciones", getSuscripcion)
-export default router 
