89deb0bc94ec15d38f2695acfb1ee0bf972ae2da
Andre-Leandro <andreleandrosanlorenzo@gmail.com>
2023-06-10 01:38:59 -0300

    :)

--- frontend/src/pages/AdmPage/AdmPage.tsx
+++ frontend/src/pages/AdmPage/AdmPage.tsx
@@ -6,8 +6,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const diasSemana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
-const valoresSelect = [ " " , 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+
 
 function AdmPage() {
   return (
@@ -42,10 +41,7 @@ function AdmPage() {
                         </Row>
                         <br />  
                 </Col>
-                   
-
                    <Col> <PaymentForm/> </Col>
-
                 </Row>       
           </Container>
 
