5e9d704b9f6ad298a16f51cfd763714eddedda23
SixtoArrejin <128509377+SixtoArrejin@users.noreply.github.com>
2023-06-11 15:12:10 -0300

    Se agrego Localidad y Provincia en nuevo establecimiento

--- frontend/src/pages/NewEstab/NewEstab.tsx
+++ frontend/src/pages/NewEstab/NewEstab.tsx
@@ -4,9 +4,10 @@ import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { FloatingLabel } from "react-bootstrap";
 
 const diasSemana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
-const valoresSelect = [ " " , 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+const valoresSelect = [" ", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
 
 
 
@@ -24,15 +25,66 @@ function NewEstab() {
           <Form.Group >
             <Container>
               <Row>
-                <Col><Form.Control type="text" placeholder="Nombre" required /></Col>
-                <Col><Form.Control type="text" placeholder="Dirección" required /></Col>
+                <Col>
+                  <FloatingLabel
+                    controlId="floatingTextarea"
+                    label="Nombre"
+                    className="mb-3"
+                  >
+                    <Form.Control type="text" placeholder="Nombre" required />
+                  </FloatingLabel>
+                </Col>
+                <Col>
+                  <FloatingLabel
+                    controlId="floatingTextarea"
+                    label="Dirección"
+                    className="mb-3"
+                  >
+                    <Form.Control type="text" placeholder="Dirección" required />
+                  </FloatingLabel>
+                </Col>
               </Row>
-              <br />
               <Row>
-                <Col><Form.Control type="email" placeholder="Correo electronico" required /></Col>
-                <Col><Form.Control type="number" placeholder="Télefono" required /></Col>
+                <Col>
+                  <FloatingLabel
+                    controlId="floatingTextarea"
+                    label="Localidad"
+                    className="mb-3"
+                  >
+                    <Form.Control type="text" placeholder="Localidad" required />
+                  </FloatingLabel>
+                </Col>
+                <Col>
+                  <FloatingLabel
+                    controlId="floatingTextarea"
+                    label="Provincia"
+                    className="mb-3"
+                  >
+                    <Form.Control type="text" placeholder="Provincia" required />
+                  </FloatingLabel>
+                </Col>
               </Row>
-              <br />
+              <Row>
+                <Col>
+                  <FloatingLabel
+                    controlId="floatingTextarea"
+                    label="Correo electronico"
+                    className="mb-3"
+                  >
+                    <Form.Control type="email" placeholder="Correo electronico" required />
+                  </FloatingLabel>
+                </Col>
+                <Col>
+                  <FloatingLabel
+                    controlId="floatingTextarea"
+                    label="Télefono"
+                    className="mb-3"
+                  >
+                    <Form.Control type="number" placeholder="Télefono" required />
+                  </FloatingLabel>
+
+                </Col>
+              </Row>
               <Row>
                 <Col>
                   <Form.Control
