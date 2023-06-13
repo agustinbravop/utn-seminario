e2bd35eb4c292e2de525baf468da207312ee2840
SixtoArrejin <128509377+SixtoArrejin@users.noreply.github.com>
2023-06-13 01:57:22 -0300

    Se agrego vista de LogIn y se arreglo Establecimientos

--- frontend/src/pages/NewEstab/NewEstab.tsx
+++ frontend/src/pages/NewEstab/NewEstab.tsx
@@ -1,51 +1,20 @@
 import "./NewEstab.css";
 import Form from 'react-bootstrap/Form';
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import TopMenu from "../../components/TopMenu";
-import { FloatingLabel } from "react-bootstrap";
 
-const diasSemana = [
-  "Lunes",
-  "Martes",
-  "Miércoles",
-  "Jueves",
-  "Viernes",
-  "Sábado",
-  "Domingo",
-];
-const valoresSelect = [
-  " ",
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  10,
-  11,
-  12,
-  13,
-  14,
-  15,
-  16,
-  17,
-  18,
-  19,
-  20,
-  21,
-  22,
-  23,
-  24,
-];
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { FloatingLabel } from "react-bootstrap";
+import TopMenu from "../../components/TopMenu";
+import { Fragment } from "react";
+import { crearEstablecimiento } from "../../utils/api";
+
+// const diasSemana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+// const valoresSelect = [" ", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
 
 function NewEstab() {
   return (
-    <div>
+    <Fragment>
       <TopMenu />
       <div className="page">
         <div className="centrado">
@@ -54,43 +23,76 @@ function NewEstab() {
         <br />
 
         <div className="formulario">
-          <Form style={{ width: "60%" }}>
-            <Form.Group>
+          <Form style={{ width: '60%' }}>
+
+            <Form.Group >
               <Container>
                 <Row>
                   <Col>
-                    <Form.Control type="text" placeholder="Nombre" required />
+                    <FloatingLabel
+                      controlId="floatingTextarea"
+                      label="Nombre"
+                      className="mb-3"
+                    >
+                      <Form.Control type="text" placeholder="Nombre" required />
+                    </FloatingLabel>
                   </Col>
                   <Col>
-                    <Form.Control
-                      type="text"
-                      placeholder="Dirección"
-                      required
-                    />
+                    <FloatingLabel
+                      controlId="floatingTextarea"
+                      label="Dirección"
+                      className="mb-3"
+                    >
+                      <Form.Control type="text" placeholder="Dirección" required />
+                    </FloatingLabel>
                   </Col>
                 </Row>
-                <br />
                 <Row>
                   <Col>
-                    <Form.Control
-                      type="email"
-                      placeholder="Correo electronico"
-                      required
-                    />
+                    <FloatingLabel
+                      controlId="floatingTextarea"
+                      label="Localidad"
+                      className="mb-3"
+                    >
+                      <Form.Control type="text" placeholder="Localidad" required />
+                    </FloatingLabel>
                   </Col>
                   <Col>
-                    <Form.Control
-                      type="number"
-                      placeholder="Télefono"
-                      required
-                    />
+                    <FloatingLabel
+                      controlId="floatingTextarea"
+                      label="Provincia"
+                      className="mb-3"
+                    >
+                      <Form.Control type="text" placeholder="Provincia" required />
+                    </FloatingLabel>
                   </Col>
                 </Row>
-                <br />
+                <Row>
+                  <Col>
+                    <FloatingLabel
+                      controlId="floatingTextarea"
+                      label="Correo electronico"
+                      className="mb-3"
+                    >
+                      <Form.Control type="email" placeholder="Correo electronico" required />
+                    </FloatingLabel>
+                  </Col>
+                  <Col>
+                    <FloatingLabel
+                      controlId="floatingTextarea"
+                      label="Télefono"
+                      className="mb-3"
+                    >
+                      <Form.Control type="number" placeholder="Télefono" required />
+                    </FloatingLabel>
+
+                  </Col>
+                </Row>
                 <Row>
                   <Col>
                     <Form.Control
                       type="file"
+                      required
                       name="file"
                       accept="image/*"
                       placeholder="Seleccionar imagen"
@@ -100,75 +102,51 @@ function NewEstab() {
                 <br />
               </Container>
             </Form.Group>
-            <br />
-            <br />
+            <br /><br />
             <div className="parrafo">
               <h2>Horarios de Atención</h2>
-              <p>En qué rango horario el establecimiento está abierto.</p>
+              <p>Describa los horarios de atención del establecimiento</p>
             </div>
+
 
             <Container>
               <Row>
                 <Col>
-                  <FloatingLabel
-                    controlId="floatingTextarea"
-                    label="Nombre"
-                    className="mb-3"
-                  >
-                    <Form.Control type="text" placeholder="Nombre" required />
-                  </FloatingLabel>
-                </Col>
-                <Col>
-                  <FloatingLabel
-                    controlId="floatingTextarea"
-                    label="Dirección"
-                    className="mb-3"
-                  >
-                    <Form.Control type="text" placeholder="Dirección" required />
-                  </FloatingLabel>
+                  <Form.Control as="textarea" rows={3} />
                 </Col>
               </Row>
-              <Row>
-                <Col>
-                  <FloatingLabel
-                    controlId="floatingTextarea"
-                    label="Localidad"
-                    className="mb-3"
-                  >
-                    <Form.Control type="text" placeholder="Localidad" required />
-                  </FloatingLabel>
-                </Col>
-                <Col>
-                  <FloatingLabel
-                    controlId="floatingTextarea"
-                    label="Provincia"
-                    className="mb-3"
-                  >
-                    <Form.Control type="text" placeholder="Provincia" required />
-                  </FloatingLabel>
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <FloatingLabel
-                    controlId="floatingTextarea"
-                    label="Correo electronico"
-                    className="mb-3"
-                  >
-                    <Form.Control type="email" placeholder="Correo electronico" required />
-                  </FloatingLabel>
-                </Col>
-                <Col>
-                  <FloatingLabel
-                    controlId="floatingTextarea"
-                    label="Télefono"
-                    className="mb-3"
-                  >
-                    <Form.Control type="number" placeholder="Télefono" required />
-                  </FloatingLabel>
+            </Container>
 
-                </Col>
+            {/*
+            <div className="parrafo">
+              <h2>Horarios de Atención</h2>
+              <p>En qué rango horario el establecimiento está abierto.</p>
+            </div>
+         
+            <Container>
+              <Row>
+                <Col></Col>
+                {diasSemana.map((dia, index) => (
+                  <Col key={index}>{dia}</Col>
+                ))}
               </Row>
+              <br />
+              <Row>
+                <Col>Desde las</Col>
+                {diasSemana.map((dia, index) => (
+                  <Col key={index}>
+                    <Form.Select aria-label="Default select example">
+                      {valoresSelect.map((option, index) => (
+                        <option key={index} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </Form.Select>
+                  </Col>
+                ))}
+              </Row>
+              <br />
+              <br />
               <Row>
                 <Col>Hasta las</Col>
                 {diasSemana.map((dia, index) => (
@@ -184,21 +162,29 @@ function NewEstab() {
                 ))}
               </Row>
               <br />
-            </Container>
+            </Container> */}
 
             <div className="centrado">
               <button
                 type="submit"
                 className="btn btn-danger"
                 style={{ backgroundColor: "#FF604F" }}
+                onClick={() => (
+                  console.log("crearEstab")
+                )}
               >
                 Crear
               </button>
+
+              <br /><br /><br /><br />
             </div>
+
           </Form>
+
         </div>
+
       </div>
-    </div>
+    </Fragment>
   );
 }
 
