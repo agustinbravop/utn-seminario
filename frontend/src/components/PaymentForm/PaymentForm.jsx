1549bf47b1158f63087c3d65434f87cea4dfaa74
Agustín Bravo Pérez <anbravoperez@gmail.com>
2023-06-13 20:03:28 -0300

    fix merge de la rama andre

--- frontend/src/components/PaymentForm/PaymentForm.jsx
+++ frontend/src/components/PaymentForm/PaymentForm.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import Cards from 'react-credit-cards';
-import 'react-credit-cards/es/styles-compiled.css'
-import Form from 'react-bootstrap/Form';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { FloatingLabel } from 'react-bootstrap';
+import React from "react";
+import Cards from "react-credit-cards";
+import "react-credit-cards/es/styles-compiled.css";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { FloatingLabel } from "react-bootstrap";
 
 export default class PaymentForm extends React.Component {
   state = {
@@ -35,92 +35,89 @@ export default class PaymentForm extends React.Component {
               <Row>
                 {" "}
                 <Col>
-                  <Form.Control
-                    type="tel"
-                    name="number"
-                    placeholder="Numero de tarjeta"
-                    onChange={this.handleInputChange}
-                    onFocus={this.handleInputFocus}
-                    required
-                  />{" "}
-                </Col>{" "}
-              </Row>
-              <br />
-              <Row>
-                {" "}
-                <Col>
-                  <Form.Control
-                    type="tel"
-                    name="name"
-                    placeholder=" Nombre "
-                    onChange={this.handleInputChange}
-                    onFocus={this.handleInputFocus}
-                    required
-                  />{" "}
-                </Col>{" "}
-              </Row>
-              <br />
-              <Row>
-                <Col>
-                    <Row>  <Col>   <FloatingLabel
+                  {" "}
+                  <FloatingLabel
                     controlId="floatingTextarea"
                     label="Numero de tarjeta"
                     type="tel"
                     className="mb-3"
-                  >  <Form.Control type="tel"
-                  name="number"
-                  placeholder="Numero de tarjeta"
-                  onChange={this.handleInputChange}
-                  onFocus={this.handleInputFocus} required /> </FloatingLabel>   </Col> </Row>
-                       
-                            <Row>  <Col>    <FloatingLabel
+                  >
+                    {" "}
+                    <Form.Control
+                      type="tel"
+                      name="number"
+                      placeholder="Numero de tarjeta"
+                      onChange={this.handleInputChange}
+                      onFocus={this.handleInputFocus}
+                      required
+                    />{" "}
+                  </FloatingLabel>{" "}
+                </Col>{" "}
+              </Row>
+
+              <Row>
+                {" "}
+                <Col>
+                  {" "}
+                  <FloatingLabel
                     controlId="floatingTextarea"
                     label="Nombre"
                     type="name"
                     className="mb-3"
-                  > <Form.Control type="tel"
-                  name="name"
-                  placeholder=" Nombre "
-                  onChange={this.handleInputChange}
-                  onFocus={this.handleInputFocus} required /> </FloatingLabel> </Col>    </Row>
-                        
-                        <Row>
-                            <Col> <FloatingLabel
+                  >
+                    {" "}
+                    <Form.Control
+                      type="tel"
+                      name="name"
+                      placeholder=" Nombre "
+                      onChange={this.handleInputChange}
+                      onFocus={this.handleInputFocus}
+                      required
+                    />{" "}
+                  </FloatingLabel>{" "}
+                </Col>{" "}
+              </Row>
+
+              <Row>
+                <Col>
+                  {" "}
+                  <FloatingLabel
                     controlId="floatingTextarea"
                     label="Valida hasta"
                     type="expiry"
                     className="mb-3"
-                  > <Form.Control  type="tel"
-                  name="expiry"
-                  placeholder="Valida hasta "
-                  onChange={this.handleInputChange}
-                  onFocus={this.handleInputFocus}  required /> </FloatingLabel>  </Col>
-                            <Col> <FloatingLabel
+                  >
+                    {" "}
+                    <Form.Control
+                      type="tel"
+                      name="expiry"
+                      placeholder="Valida hasta "
+                      onChange={this.handleInputChange}
+                      onFocus={this.handleInputFocus}
+                      required
+                    />{" "}
+                  </FloatingLabel>{" "}
+                </Col>
+                <Col>
+                  {" "}
+                  <FloatingLabel
                     controlId="floatingTextarea"
                     label="CVV"
                     type="cvv"
                     className="mb-3"
-                  >  <Form.Control type="tel"
-                  name="cvc"
-                  placeholder="CVV "
-                  onChange={this.handleInputChange}
-                  onFocus={this.handleInputFocus} required /> </FloatingLabel> </Col>
-                        </Row>
-                        
-                </Col>
-                <Col>
-                  {" "}
-                  <Form.Control
-                    type="tel"
-                    name="cvc"
-                    placeholder="CVV "
-                    onChange={this.handleInputChange}
-                    onFocus={this.handleInputFocus}
-                    required
-                  />{" "}
+                  >
+                    {" "}
+                    <Form.Control
+                      type="tel"
+                      name="cvc"
+                      placeholder="CVV "
+                      onChange={this.handleInputChange}
+                      onFocus={this.handleInputFocus}
+                      required
+                    />{" "}
+                  </FloatingLabel>{" "}
                 </Col>
               </Row>
-              <br />
             </Col>
             <Col>
               <Cards
--- frontend/src/pages/AdmPage/AdmPage.tsx
+++ frontend/src/pages/AdmPage/AdmPage.tsx
@@ -10,7 +10,6 @@ import { useState } from "react";
 import { JSX } from "react/jsx-runtime";
 import { FloatingLabel } from "react-bootstrap";
 
-
 function AdmPage() {
   const [showModal, setShowModal] = useState(false);
   const renderBackdrop = (
@@ -18,273 +17,225 @@ function AdmPage() {
       React.ClassAttributes<HTMLDivElement> &
       React.HTMLAttributes<HTMLDivElement>
   ) => <div className="backdrop" {...props} />;
-  const navigate = useNavigate();
-  const handleClose = () => setShowModal(false);
-  const handleSuccess = () => {
-    // TODO: sadsa
+  var handleClose = () => setShowModal(false);
+  var handleSuccess = () => {
     console.log(":)");
   };
 
   return (
-    <>
-      <TopMenu />
-      <div className="page">
-        <div className="margen">
-          <h2>Tarjeta de credito</h2>
-          <p>
-            {" "}
-            Se factura una cuota cada 30 días. Se puede dar de baja en cualquier
-            momento.
-          </p>
-        </div>
+    <div className="page">
+      <div className="margen">
+        <h2>Tarjeta de credito</h2>
+        <p>
+          {" "}
+          Se factura una cuota cada 30 días. Se puede dar de baja en cualquier
+          momento.
+        </p>
+      </div>
 
-        <br />
-        <br />
+      <br />
+      <br />
+
+      <div className="formulario">
+        <Form style={{ width: "50%", marginLeft: "4%" }}>
+          <Form.Group>
+            <PaymentForm />
+          </Form.Group>
+        </Form>
+      </div>
+      <br />
 
       <div className="margen">
         <h2>Cuenta</h2>
         <p> Ingrese los datos a usar para iniciar sesión.</p>
       </div>
 
-    <br />
-    <br />
+      <br />
+      <br />
 
-            <div className="formulario">
-            <Form style={{ width: '34%' }}>
-
-              <Form.Group >
-                <Container>
-                    <Row>
-                        <Col>  <FloatingLabel
+      <div className="formulario">
+        <Form style={{ width: "34%" }}>
+          <Form.Group>
+            <Container>
+              <Row>
+                <Col>
+                  {" "}
+                  <FloatingLabel
                     controlId="floatingTextarea"
                     label="Nombre"
                     className="mb-3"
-                  > <Form.Control type="text" placeholder="Nombre" required /> </FloatingLabel> </Col>
-                        <Col>  <FloatingLabel
-                    controlId="floatingTextarea"
-                    label="Apellido"
-                    className="mb-3"
-                  >  <Form.Control type="text" placeholder="Apellido" required /> </FloatingLabel> </Col>
-                    </Row>
-                    
-                    <Row>  <Col>    <FloatingLabel
-                    controlId="floatingTextarea"
-                    label="Teléfono"
-                    className="mb-3"
-                  >   <Form.Control type="text" placeholder="Teléfono" required />  </FloatingLabel>     </Col> </Row>
-                   
-                    <Row>  <Col>   <FloatingLabel
-                    controlId="floatingTextarea"
-                    label="Nombre de usuario"
-                    className="mb-3"
-                  > <Form.Control type="text" placeholder="Nombre de usuario" required />   </FloatingLabel>      </Col> </Row>
-                  
-                   
-                    <Row>  <Col>    
-                    
-                    <FloatingLabel
-                    controlId="floatingTextarea"
-                    label="Correo electronico"
-                    className="mb-3"
                   >
-                    <Form.Control type="text" placeholder="Nombre" required />
-                  </FloatingLabel> 
-                    
-                    </Col> </Row>
-              
-
-                    
-                        <Row>  <Col>   <FloatingLabel
-                    controlId="floatingTextarea"
-                    label="Contraseña"
-                    className="mb-3"
-                  > <Form.Control type="password" placeholder="Contraseña" required />  </FloatingLabel>   </Col>    </Row>
-                    <br />
-                    
-                    <br />
-                          
-                </Container>
-
-        <div className="margen">
-          <h2>Cuenta</h2>
-          <p> Ingrese los datos a usar para iniciar sesión.</p>
-        </div>
-
-        <br />
-        <br />
-
-        <div className="formulario">
-          <Form style={{ width: "34%" }}>
-            <Form.Group>
-              <Container>
-                <Row>
-                  <Col>
                     {" "}
                     <Form.Control
                       type="text"
                       placeholder="Nombre"
                       required
                     />{" "}
-                  </Col>
-                  <Col>
+                  </FloatingLabel>{" "}
+                </Col>
+                <Col>
+                  {" "}
+                  <FloatingLabel
+                    controlId="floatingTextarea"
+                    label="Apellido"
+                    className="mb-3"
+                  >
                     {" "}
                     <Form.Control
                       type="text"
                       placeholder="Apellido"
                       required
                     />{" "}
-                  </Col>
-                </Row>
-                <br />
-                <Row>
+                  </FloatingLabel>{" "}
+                </Col>
+              </Row>
+
+              <Row>
+                {" "}
+                <Col>
                   {" "}
-                  <Col>
-                    <Form.Control type="text" placeholder="Teléfono" required />{" "}
-                  </Col>{" "}
-                </Row>
-                <br />
-                <Row>
+                  <FloatingLabel
+                    controlId="floatingTextarea"
+                    label="Teléfono"
+                    className="mb-3"
+                  >
+                    {" "}
+                    <Form.Control
+                      type="text"
+                      placeholder="Teléfono"
+                      required
+                    />{" "}
+                  </FloatingLabel>{" "}
+                </Col>{" "}
+              </Row>
+
+              <Row>
+                {" "}
+                <Col>
                   {" "}
-                  <Col>
+                  <FloatingLabel
+                    controlId="floatingTextarea"
+                    label="Nombre de usuario"
+                    className="mb-3"
+                  >
+                    {" "}
                     <Form.Control
                       type="text"
                       placeholder="Nombre de usuario"
                       required
                     />{" "}
-                  </Col>{" "}
-                </Row>
-                <br />
+                  </FloatingLabel>{" "}
+                </Col>{" "}
+              </Row>
 
-                <Row>
+              <Row>
+                {" "}
+                <Col>
+                  <FloatingLabel
+                    controlId="floatingTextarea"
+                    label="Correo electronico"
+                    className="mb-3"
+                  >
+                    <Form.Control type="text" placeholder="Nombre" required />
+                  </FloatingLabel>
+                </Col>{" "}
+              </Row>
+
+              <Row>
+                {" "}
+                <Col>
                   {" "}
-                  <Col>
-                    <Form.Control
-                      type="email"
-                      placeholder="Correo electrónico"
-                      required
-                    />{" "}
-                  </Col>{" "}
-                </Row>
-                <br />
-                <Row>
-                  {" "}
-                  <Col>
+                  <FloatingLabel
+                    controlId="floatingTextarea"
+                    label="Contraseña"
+                    className="mb-3"
+                  >
+                    {" "}
                     <Form.Control
                       type="password"
                       placeholder="Contraseña"
                       required
                     />{" "}
-                  </Col>{" "}
-                </Row>
-                <br />
+                  </FloatingLabel>{" "}
+                </Col>{" "}
+              </Row>
+              <br />
 
-                <br />
-              </Container>
-            </Form.Group>
+              <br />
+            </Container>
+          </Form.Group>
 
-            <div className="centrado">
-              <button
-                type="submit"
-                className="btn btn-danger"
-                style={{ backgroundColor: "#249AD5", borderColor: "#249AD5" }}
-                onClick={() => navigate("/landing")}
-              >
-                Registrarme
-              </button>
-            </div>
-          </Form>
-        </div>
-
-        <Modal
-          className="modal"
-          show={showModal}
-          onHide={handleClose}
-          renderBackdrop={renderBackdrop}
-        >
-          <div>
-            <div className="modal-header">
-              <div className="modal-title">Confirmar Cancelación</div>
-              <div>
-                <span className="close-button" onClick={handleClose}>
-                  x
-                </span>
-              </div>
-            </div>
-            <div className="modal-desc">
-              <p>¿Desea cancelar?</p>
-            </div>
-            <div className="modal-footer">
-              <button className="secondary-button" onClick={handleClose}>
-                Cancelar
-              </button>
-              <button className="primary-button" onClick={handleSuccess}>
-                Aceptar
-              </button>
-            </div>
+          <div className="centrado">
+            <button
+              type="submit"
+              className="btn btn-danger"
+              style={{ backgroundColor: "#249AD5", borderColor: "#249AD5" }}
+              onClick={() => console.log(":(")}
+            >
+              Registrarse
+            </button>
           </div>
-        </Modal>
+        </Form>
       </div>
 
-            <Modal
-                    className="modal"
-                    show={showModal}
-                    onHide={handleClose}
-                    renderBackdrop={renderBackdrop}
-                  >
-                    <div>
-                      <div className="modal-header">
-                        <div className="modal-title">Confirmar Cancelación</div>
-                        <div>
-                          <span className="close-button" onClick={handleClose}>
-                            x
-                          </span>
-                        </div>
-                      </div>
-                      <div className="modal-desc">
-                        <p>¿Desea cancelar?</p>
-                      </div>
-                      <div className="modal-footer">
-                        <button className="secondary-button" onClick={handleClose}>
-                          Cancelar
-                        </button>
-                        <button className="primary-button" onClick={handleSuccess}>
-                          Aceptar
-                        </button>
-                      </div>
-                    </div>
-                </Modal>
+      <Modal
+        className="modal"
+        show={showModal}
+        onHide={handleClose}
+        renderBackdrop={renderBackdrop}
+      >
+        <div>
+          <div className="modal-header">
+            <div className="modal-title">Confirmar Cancelación</div>
+            <div>
+              <span className="close-button" onClick={handleClose}>
+                x
+              </span>
+            </div>
+          </div>
+          <div className="modal-desc">
+            <p>¿Desea cancelar?</p>
+          </div>
+          <div className="modal-footer">
+            <button className="secondary-button" onClick={handleClose}>
+              Cancelar
+            </button>
+            <button className="primary-button" onClick={handleSuccess}>
+              Aceptar
+            </button>
+          </div>
+        </div>
+      </Modal>
 
-                
-            <Modal
-                    className="modal"
-                    show={showModal}
-                    onHide={handleClose}
-                    renderBackdrop={renderBackdrop}
-                  >
-                    <div>
-                      <div className="modal-header">
-                        <div className="modal-title">Confirmar Cancelación</div>
-                        <div>
-                          <span className="close-button" onClick={handleClose}>
-                            x
-                          </span>
-                        </div>
-                      </div>
-                      <div className="modal-desc">
-                        <p>¿Desea cancelar?</p>
-                      </div>
-                      <div className="modal-footer">
-                        <button className="secondary-button" onClick={handleClose}>
-                          Cancelar
-                        </button>
-                        <button className="primary-button" onClick={handleSuccess}>
-                          Aceptar
-                        </button>
-                      </div>
-                    </div>
-                </Modal>
-
-  </div>
+      <Modal
+        className="modal"
+        show={showModal}
+        onHide={handleClose}
+        renderBackdrop={renderBackdrop}
+      >
+        <div>
+          <div className="modal-header">
+            <div className="modal-title">Confirmar Cancelación</div>
+            <div>
+              <span className="close-button" onClick={handleClose}>
+                x
+              </span>
+            </div>
+          </div>
+          <div className="modal-desc">
+            <p>¿Desea cancelar?</p>
+          </div>
+          <div className="modal-footer">
+            <button className="secondary-button" onClick={handleClose}>
+              Cancelar
+            </button>
+            <button className="primary-button" onClick={handleSuccess}>
+              Aceptar
+            </button>
+          </div>
+        </div>
+      </Modal>
+    </div>
   );
 }
 export default AdmPage;
