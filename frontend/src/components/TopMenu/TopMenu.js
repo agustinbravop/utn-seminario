dc173f7be880b607cd8a426e008b72af7e2c41d1
Aldo <aldoandres045@gmail.com>
2023-06-21 11:24:31 -0300

    Agrego nombre de usuario en NavBar

--- frontend/src/components/TopMenu/TopMenu.js
+++ frontend/src/components/TopMenu/TopMenu.js
@@ -3,15 +3,33 @@ import { Container, Navbar, Nav, Button } from "react-bootstrap";
 import { ReactComponent as Logo } from "../../assets/svg/tennis-icon.svg";
 import "./TopMenu.scss";
 import { Link } from "react-router-dom";
+import { readLocalStorage } from "../../utils/storage/localStorage";
+
 
 export default function TopMenu() {
+
+
+
+  const info = readLocalStorage('token');
+  function decodeJWT(token) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    return JSON.parse(jsonPayload);
+}
   
-  
+
+  const data = decodeJWT(info)
+  console.log(data)
+
   return (
     <Navbar bg="dark" variant="dark" className="top-menu">
       <Container>
         <BrandNav/>
         <MenuNav />
+        {data.usuario.usuario && <button className="btn btn-light rounded-pill btn-sm">{data.usuario.usuario}</button>}
       </Container>
     </Navbar>
   );
