0a8708da41298d68547fa56b33ce2d0f5dbd459b
Agustín Bravo Pérez <anbravoperez@gmail.com>
2023-06-21 18:30:02 -0300

    usado useCurrentAdmin en TopMenu

--- frontend/src/components/TopMenu/TopMenu.js
+++ frontend/src/components/TopMenu/TopMenu.js
@@ -3,33 +3,23 @@ import { Container, Navbar, Nav, Button } from "react-bootstrap";
 import { ReactComponent as Logo } from "../../assets/svg/tennis-icon.svg";
 import "./TopMenu.scss";
 import { Link } from "react-router-dom";
-import { readLocalStorage } from "../../utils/storage/localStorage";
-
+import { useCurrentAdmin } from "../../hooks/useCurrentAdmin";
 
 export default function TopMenu() {
-
-
-
-  const info = readLocalStorage('token');
-  function decodeJWT(token) {
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-    return JSON.parse(jsonPayload);
-}
-  
-
-  const data = decodeJWT(info)
-  console.log(data)
+  const { currentAdmin, logout } = useCurrentAdmin();
 
   return (
     <Navbar bg="dark" variant="dark" className="top-menu">
       <Container>
-        <BrandNav/>
+        <BrandNav />
         <MenuNav />
-        {data.usuario.usuario && <button className="btn btn-light rounded-pill btn-sm">{data.usuario.usuario}</button>}
+        {currentAdmin && (
+          <button
+            className="btn btn-light rounded-pill btn-sm"
+          >
+            {currentAdmin.usuario}
+          </button>
+        )}
       </Container>
     </Navbar>
   );
