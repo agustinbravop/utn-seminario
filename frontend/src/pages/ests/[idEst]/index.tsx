c95afce1a6128eccabee072d28d3ebc35d756ac1
Andre-Leandro <andreleandrosanlorenzo@gmail.com>
2023-08-01 19:15:36 -0300

    Ajustes de posiciones y desfasajes

--- frontend/src/pages/ests/[idEst]/index.tsx
+++ frontend/src/pages/ests/[idEst]/index.tsx
@@ -8,17 +8,14 @@ import {
   CardBody,
   HStack,
   Heading,
-  Input,
-  InputGroup,
-  InputRightElement,
   Stack,
   StackDivider,
   Text,
 } from "@chakra-ui/react";
 import { getEstablecimientoByID } from "@/utils/api/establecimientos";
-import EstabPage from "./_estabPage";
+import SubMenu from "@/components/SubMenu/SubMenu";
 import { Image } from "@chakra-ui/react";
-import { EditIcon, SearchIcon } from "@chakra-ui/icons";
+import { EditIcon} from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
 
 export default function CourtPage() {
@@ -29,108 +26,77 @@ export default function CourtPage() {
   );
 
   return (
-    <div>
-      <EstabPage />
-      <Heading
-        size="md"
-        fontSize="26px"
-        textAlign="left"
-        marginLeft="18%"
-        marginTop="20px"
-      >
-        Información
-      </Heading>
-      <HStack
-        marginRight="auto"
-        marginLeft="18%"
-        marginBottom="50px"
-        marginTop="20px"
-      >
-        <InputGroup width="18%">
-          <InputRightElement pointerEvents="none">
-            <SearchIcon color="gray.300" />
-          </InputRightElement>
-          <Input
-            focusBorderColor="lightblue"
-            placeholder="Nombre de la cancha"
-            size="md"
-            width="100%"
-          />
-        </InputGroup>
-        <HStack
-          marginLeft="auto"
-          marginRight="10%"
-          display="flex"
-          alignContent="column"
-          spacing={5}
-          align="center"
-        >
-          <Link to="editar">
-            <Button leftIcon={<EditIcon />}>Editar Info</Button>
-          </Link>
-        </HStack>
-      </HStack>
-      <Box display="flex" marginLeft="18%" height="70%">
+    <>
+      <SubMenu/>
+      <Heading size="md" fontSize="26px" textAlign="left" marginLeft="18%" marginTop="20px" > Información </Heading>
+      <HStack marginRight="auto" marginLeft="18%" marginBottom="1px" marginTop="20px" >
+            <HStack marginLeft="auto" marginRight="10%" display="flex" alignContent="column" spacing={5} align="center" >
+            <Link to="editar">
+              <Button leftIcon={<EditIcon />}>Editar Info</Button>
+            </Link>
+            </HStack> 
+            </HStack>
+      <Box
+      display="flex"
+      marginLeft="18%" 
+      height="70%" 
+    >
         <Card
-          boxSize="43rem"
-          justifyContent="center"
-          display="flex"
-          style={{ marginTop: "10px", marginBottom: "1rem" }}
-          height="75%"
-          width="73%"
+        boxSize="43rem"
+        justifyContent="center"
+        display="flex"
+        style={{ marginTop:"10px", marginBottom: "1rem" }}
+        height="75%"
+        width="73%"
         >
-          <CardBody height="100%" marginTop="0px">
-            <Box display="grid" gridTemplateColumns="1fr 1fr" height="100%">
-              <Box>
-                <Image
-                  src={data?.urlImagen}
-                  width="1000px"
-                  height="400px"
-                  objectFit="cover"
-                  borderRadius="10px"
-                />
-              </Box>
-
-              <Box marginTop="55px" marginLeft=" 50px" height="100%">
-                <Stack divider={<StackDivider />} spacing="1" marginTop="-2rem">
-                  <Box>
-                    <Heading size="xs" textTransform="uppercase">
-                      Dirección
-                    </Heading>
-                    <Text fontSize="sm">{data?.direccion}</Text>
-                  </Box>
-                  <Box>
-                    <Heading size="xs" textTransform="uppercase">
-                      Horario atencion
-                    </Heading>
-                    <Text fontSize="sm">{data?.horariosDeAtencion}</Text>
-                  </Box>
-                  <Box>
-                    <Heading size="xs" textTransform="uppercase">
-                      Correo de contacto
-                    </Heading>
-                    <Text fontSize="sm">{data?.correo}</Text>
-                  </Box>
-                  <Box>
-                    <Heading size="xs" textTransform="uppercase">
-                      Numero de teléfono
-                    </Heading>
-                    <Text fontSize="sm">{data?.telefono}</Text>
-                  </Box>
-                  <Box>
-                    <Heading size="xs" textTransform="uppercase">
-                      Localidad
-                    </Heading>
-                    <Text fontSize="sm">
-                      {data?.localidad}, {data?.provincia}
-                    </Text>
-                  </Box>
-                </Stack>
-              </Box>
-            </Box>
-          </CardBody>
-        </Card>
-      </Box>
-    </div>
+            <CardBody height="100%" marginTop="0px">
+                <Box display="grid" gridTemplateColumns="1fr 1fr" height="100%">
+                        <Box >
+                            <Image src={data?.urlImagen} width="1000px"  height="400px" objectFit="cover" borderRadius="10px"/>
+                        </Box>
+                        
+                        <Box marginTop="55px" marginLeft=" 50px" height="100%">
+                            <Stack divider={<StackDivider />} spacing="1" marginTop="-2rem">
+                                <Box>
+                                <Heading size="xs" textTransform="uppercase">
+                                    Dirección
+                                </Heading>
+                                <Text fontSize="sm">
+                                    {data?.direccion}
+                                </Text>
+                                </Box>
+                                <Box>
+                                <Heading size="xs" textTransform="uppercase">
+                                    Horario atencion
+                                </Heading>
+                                <Text fontSize="sm">{data?.horariosDeAtencion}</Text>
+                                </Box>
+                                <Box>
+                                <Heading size="xs" textTransform="uppercase">
+                                    Correo de contacto
+                                </Heading>
+                                <Text fontSize="sm">{data?.correo}</Text>
+                                </Box>
+                                <Box>
+                                <Heading size="xs" textTransform="uppercase">
+                                    Numero de teléfono
+                                </Heading>
+                                <Text fontSize="sm">{data?.telefono}</Text>
+                                </Box>
+                                <Box>
+                                <Heading size="xs" textTransform="uppercase">
+                                Localidad
+                                </Heading>
+                                <Text fontSize="sm">
+                                    {data?.localidad}, {data?.provincia}
+                                </Text>
+                                </Box>
+                            </Stack>
+                        </Box>
+                </Box>
+            </CardBody>
+            </Card>
+    </Box>
+    </>
   );
 }
