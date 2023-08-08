eb647eed47e778251a120c29acfcfa6331e5fdd0
Andre-Leandro <andreleandrosanlorenzo@gmail.com>
2023-08-08 13:27:41 -0300

    Modificacion de imagen y esquema bd, permite correos identicos

--- frontend/src/utils/const/const.ts
+++ frontend/src/utils/const/const.ts
@@ -1,1 +1,1 @@
-export const defImage = "https://t3.ftcdn.net/jpg/04/84/88/76/360_F_484887682_Mx57wpHG4lKrPAG0y7Q8Q7bJ952J3TTO.jpg";
+export const defImage = "https://cdn3.vectorstock.com/i/1000x1000/97/37/photo-camera-icon-vector-21679737.jpg";
