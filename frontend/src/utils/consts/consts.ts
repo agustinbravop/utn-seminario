c36a30f4a81c6f6fe42211f959a6062cb6e83f8b
Agustín Bravo Pérez <anbravoperez@gmail.com>
2023-08-30 18:52:42 -0300

    refactor: carpeta de constantes

---
+++ frontend/src/utils/consts/consts.ts
@@ -0,0 +1,39 @@
+export const DEFAULT_IMG =
+  "https://static.vecteezy.com/system/resources/thumbnails/004/141/669/small/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg";
+
+export const DISCIPLINAS = [
+  "Basket",
+  "Futbol",
+  "Tenis",
+  "Padel",
+  "Hokey",
+  "Ping-Pong",
+];
+
+export const DURACION_RESERVA = [30, 60];
+
+export const HORAS = [
+  "1:00",
+  "2:00",
+  "3:00",
+  "4:00",
+  "5:00",
+  "6:00",
+  "7:00",
+  "8:00",
+  "9:00",
+  "10:00",
+  "11:00",
+  "12:00",
+  "13:00",
+  "14:00",
+  "15:00",
+  "16:00",
+  "17:00",
+  "18:00",
+  "19:00",
+  "20:00",
+  "21:00",
+  "22:00",
+  "23:00",
+];
