f8ed7fc849f9c940618380b5224534d613d2d0b3
Aldo <aldoandres045@gmail.com>
2023-09-20 09:50:04 -0300

    tabla con filtros en proceso

---
+++ frontend/src/components/DisponibilidadesTable/DisponibilidadesTable.tsx
@@ -0,0 +1,89 @@
+import * as React from "react";
+import { Table, Thead, Tbody, Tr, Th, Td, chakra } from "@chakra-ui/react";
+import { TriangleDownIcon, TriangleUpIcon } from "@chakra-ui/icons";
+import {
+  useReactTable,
+  flexRender,
+  getCoreRowModel,
+  ColumnDef,
+  SortingState,
+  getSortedRowModel,
+} from "@tanstack/react-table";
+import { Disponibilidad } from "@/models";
+import FormReservarDisponibilidad from "@/pages/jugador/[idJugador]/est/[idEst]/canchas/[idCancha]/_formReservar";
+
+export type DataTableProps<Data extends object> = {
+  data: Data[];
+  columns: ColumnDef<Data, any>[];
+};
+
+export function DisponibilidadesTable<Data extends object>({
+  data,
+  columns,
+}: DataTableProps<Data>) {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const table = useReactTable({
+    columns,
+    data,
+    getCoreRowModel: getCoreRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
+  });
+
+  return (
+    <Table>
+      <Thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <Tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => {
+              const meta: any = header.column.columnDef.meta;
+              return (
+                <Th
+                  key={header.id}
+                  onClick={header.column.getToggleSortingHandler()}
+                  isNumeric={meta?.isNumeric}
+                >
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+
+                  <chakra.span pl="4">
+                    {header.column.getIsSorted() ? (
+                      header.column.getIsSorted() === "desc" ? (
+                        <TriangleDownIcon aria-label="sorted descending" />
+                      ) : (
+                        <TriangleUpIcon aria-label="sorted ascending" />
+                      )
+                    ) : null}
+                  </chakra.span>
+                </Th>
+              );
+            })}
+          </Tr>
+        ))}
+      </Thead>
+      <Tbody>
+        {table.getRowModel().rows.map((row) => (
+          <Tr key={row.id}>
+            {row.getVisibleCells().map((cell) => {
+              const meta: any = cell.column.columnDef.meta;
+              return (
+                <Td key={cell.id} isNumeric={meta?.isNumeric}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </Td>
+              );
+              
+            })}
+            <Td>
+              <FormReservarDisponibilidad disp={row.getValue("reserva")} />
+            </Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+  );
+}
--- frontend/src/pages/jugador/[idJugador]/est/[idEst]/reservar.tsx
+++ frontend/src/pages/jugador/[idJugador]/est/[idEst]/reservar.tsx
@@ -17,7 +17,22 @@ import { DIAS_ABBR } from "@/utils/consts";
 import FormReservarDisponibilidad from "./canchas/[idCancha]/_formReservar";
 import LoadingSpinner from "@/components/LoadingSpinner/LoadingSpinner";
 import { useState, useEffect } from "react";
-import { Cancha } from "@/models";
+import { Cancha, Dia } from "@/models";
+import { createColumnHelper } from "@tanstack/react-table";
+import { DisponibilidadesTable } from "@/components/DisponibilidadesTable/DisponibilidadesTable";
+
+type Columnas = {
+    cancha: string;
+    id: number;
+    horaInicio: string;
+    horaFin: string;
+    precioReserva: number;
+    precioSenia?: number | undefined;
+    disciplina: string;
+    dias: Dia[];
+    idCancha: number;
+}
+
 
 // TODO: falta filtrar por disponibilidades ya ocupadas en la fecha dada.
 /** Genera las filas de disponibilidades que se muestran en la tabla para reservar. */
@@ -52,6 +67,39 @@ export default function ReservarEstablecimiento() {
 
   // Se determina las filas de la tabla de disponibilidades para reservar.
   const disponibilidades = construirDisponibilidades(canchas);
+
+  //Creo la funcion que me aydua a filtrar la tabla
+  const columnHelper = createColumnHelper<Columnas>();
+
+  //Asigno las columnas que se van a poder ordenar
+  const columns = [
+    columnHelper.accessor("horaInicio", {
+      cell: (info) => info.getValue(),
+      header: "Inicio"
+    }),
+    columnHelper.accessor("horaFin", {
+      cell: (info) => info.getValue(),
+      header: "Fin"
+    }),
+    columnHelper.accessor("precioReserva", {
+      cell: (info) => ("$" + info.getValue()),
+      header: "Precio",
+    }),
+    columnHelper.accessor("precioSenia", {
+      cell: (info) => ( info.getValue()? "$" + info.getValue() : "Sin seña"),
+      header: "Seña",
+    }),
+    columnHelper.accessor("dias", {
+      cell: (info) => info.getValue().map(d => DIAS_ABBR[d]).join(", "),
+      header: "Dias"
+    }),
+    columnHelper.accessor("cancha", {
+      cell: (info) => info.getValue(),
+      header: "Cancha"
+    }),
+    columnHelper.accessor((r => r), {header:"", id: "reserva"})
+  ];
+  
 
   return (
     <>
@@ -107,6 +155,7 @@ export default function ReservarEstablecimiento() {
           </Tbody>
         </Table>
       </TableContainer>
+      <DisponibilidadesTable columns={columns} data={disponibilidades}/>
     </>
   );
 }
