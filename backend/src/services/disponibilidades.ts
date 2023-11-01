ff0190cbe28371ab7759e5b9ba3dbd1b24c215d7
Agustín Bravo Pérez <anbravoperez@gmail.com>
2023-11-01 14:03:54 -0300

    fix: validación de horarios solapados al editar una disponibilidad

--- backend/src/services/disponibilidades.ts
+++ backend/src/services/disponibilidades.ts
@@ -75,6 +75,8 @@ export class DisponibilidadServiceimpl implements DisponibilidadService {
     const disponibilidades = await this.repo.getByCanchaID(disp.idCancha);
     for (const d of disponibilidades) {
       if (
+        // Se excluye a sí misma de la validación.
+        d.id !== disp.id && 
         // Dos disponibilidades se solapan si tienen la misma disciplina,
         d.disciplina === disp.disciplina &&
         // en el mismo día de la semana,
